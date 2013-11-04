e6a6f0724a007260e523babfb6762ef253a94578
elmarxista <leonelsoriano3@gmail.com>
2013-11-03 23:35:03 -0430

    accept_terms_service

---
+++ SystenOperation.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# -*- Mode: Python; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- #
+
+class SystemOperation(WapperUrl):
+	def __init__(self,ref_session):
+		self.__session_token = ref_session
--- UserOperation.py
+++ UserOperation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # -*- Mode: Python; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- #
+import re
 from py3mediafire.BaseUrlMediaFire import BaseUrlMediaFire
 from py3mediafire.WapperUrl import WapperUrl
 
@@ -10,8 +11,8 @@ class UserOperation(WapperUrl):
 	"""
 	def __init__(self,ref_session):
 		self.__session_token = ref_session
-		s = self.get_info()
-		self.set_info({})
+		self.accept_terms_service()
+
 
 	def get_info(self):
 		js = self.get_json_mediafire(BaseUrlMediaFire.GET_INFO_USER,
@@ -39,3 +40,51 @@ class UserOperation(WapperUrl):
 		option['session_token'] = self.__session_token.get_token()
 		option['version'] = self.__session_token.get_version_actual()
 		self.get_json_mediafire(BaseUrlMediaFire.SET_INFO_USER,option)
+
+	def myfiles_revision(self):
+		"""Returns a fraction number indicating the global revision of myfiles. The revision
+		is in the x.y format. 'x' is the folders-only revision. 'y' is the folders-and-files
+		revision. When the revision resets to 1.0, the time stamp 'epoch' is updated so
+		both 'revision' and 'epoch' can be used to identify a unique revision
+		"""
+		return self.get_json_mediafire(BaseUrlMediaFire.MYFILES_REVISION_USER,
+			{'session_token': self.__session_token.get_token(),
+			'version': self.__session_token.get_version_actual()})
+
+	def get_text_terms_service(self,html=True):
+		"""Returns the HTML format of the MediaFire Terms of Service and its revision,
+		date, whether the user has accepted it or not, and the acceptance token if the
+		user has not accepted the latest terms
+
+		Keyword arguments:
+			html -- return html tag(defaul True)
+
+		Return text of terms of service
+		"""
+		js = self.get_json_mediafire(BaseUrlMediaFire.FETCH_TOS_USER,
+			{'session_token': self.__session_token.get_token(),
+			'version': self.__session_token.get_version_actual()})
+		terms = js['terms_of_service']['terms']
+		if(not html):
+			return re.sub('<.*?>','',terms)
+		return terms
+
+	def accept_terms_service(self):
+		js = self.get_json_mediafire(BaseUrlMediaFire.FETCH_TOS_USER,
+			{'session_token': self.__session_token.get_token(),
+			'version': self.__session_token.get_version_actual()})
+		token_accept = js['terms_of_service']['acceptance_token']
+		result = self.get_json_mediafire(BaseUrlMediaFire.ACCEPT_TOS_USER,
+			{'session_token': self.__session_token.get_token(),
+			'acceptance_token' : token_accept,
+			'version': self.__session_token.get_version_actual()})
+		if not __debug__:
+			print(' terms service ' + result['result'] +
+				' in UserOperation::accept_terms_service')
+
+	def set_register(self,ref_session):
+		"""Set actual user mediafire
+		Keyword arguments:
+			ref_session : reference class SessionHandler
+		"""
+		self.__session_token = ref_session
