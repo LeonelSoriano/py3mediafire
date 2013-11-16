12b33050c2d7558c2af15df9d10508d16792772d
elmarxista <leonelsoriano3@gmail.com>
2013-11-16 09:27:35 -0430

    actualizacion de datos

--- SystemOperation.py
+++ SystemOperation.py
@@ -12,16 +12,12 @@ import json
 import email.generator
 from py3mediafire.WapperUrl import WapperUrl
 from py3mediafire.BaseUrlMediaFire import BaseUrlMediaFire
+from py3mediafire.MultiPartForm import MultiPartForm
 
 #TODO add Resumable Uploads
 class SystemOperation(WapperUrl):
 	def __init__(self,ref_session):
 		self.__session_token = ref_session
-
-#		for a in range(len(reps)):
-#			#print(reps[a])
-		self.get_mime_types()
-
 
 	def get_content(self):
 		""" Return either a list of folders or a list of files."""
@@ -329,8 +325,11 @@ class SystemOperation(WapperUrl):
 		option = {}
 		option['session_token'] = self.__session_token.get_token()
 		option['folder_key'] = folder_keys
-		js = self.get_json_mediafire(BaseUrlMediaFire.ATTACH_FOREIGN_FOLDER,option)
-		return js
+		get = urllib.parse.urlencode(option)
+		url = BaseUrlMediaFire.ATTACH_FOREIGN_FOLDER + '?' + get
+		req1 = urllib.request.Request(url)
+		response = urllib.request.urlopen(req1)
+		print(response)
 
 	#TODO return access deneged 403 (bug)
 	def detach_foreign(self, folder_key):
@@ -419,7 +418,7 @@ class SystemOperation(WapperUrl):
 			print(str(js) + 'SystemOperation::poll_upload')
 		return js
 
-	def download(self, quick_key,new_name = None):
+	def download(self, quick_key,new_name = None, path = ''):
 		extension = ''
 		js_link = self.get_links(quick_key, 'direct_download')
 		if(js_link['quickkey'] == ''):
@@ -435,10 +434,17 @@ class SystemOperation(WapperUrl):
 				extension = tmp + extension
 			new_name = new_name.replace('.','')
 			name_file = new_name + extension
-			print(name_file)
-		response = urllib.request.urlopen(js_link['direct_download'])
-		with open(name_file, 'b+w') as f:
-			f.write(response.read())
+
+		response = None
+		try:
+			response = urllib.request.urlopen(js_link['direct_download'])
+		except urllib.error.URLError as e:
+			return {'result': e}
+		try:
+			with open( os.path.join(path, name_file), 'b+w') as f:
+				f.write(response.read())
+		except (OSError, IOError) as e:
+			return {'result': e}
 		return {'result': 'Success'}
 
 	def get_info_system(self):
@@ -466,83 +472,5 @@ class SystemOperation(WapperUrl):
 		js = self.get_json_mediafire(BaseUrlMediaFire.GET_MIME_TYPES,
 			{'version': self.__session_token.get_version_actual()})
 		return js
-
-
-"""
-Doug Hellmann's urllib2, translated to python3.
-"""
-class MultiPartForm():
-	"""Accumulate the data to be used when posting a form."""
-
-	def __init__(self):
-		self.form_fields = []
-		self.files = []
-		self.form_data = None
-		self.boundary = email.generator._make_boundary()
-		return
-
-	def get_content_type(self):
-		return 'multipart/form-data; boundary=%s' % self.boundary
-
-	def add_field(self, name, value):
-		"""Add a simple field to the form data."""
-		self.form_fields.append((name, value))
-		return
-
-	def add_file(self, fieldname, filename, fileHandle, mimetype=None):
-		"""Add a file to be uploaded."""
-		body = fileHandle.read()
-		if mimetype is None:
-			mimetype = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-		self.files.append((fieldname, filename, mimetype, body))
-		return
-
-	def make_result(self):
-		"""Return bytes representing the form data, including attached files."""
-        # Build a list of lists, each containing "lines" of the
-        # request. Each part is separated by a boundary string.
-        # Once the list is built, return a string where each
-        # line is separated by '\r\n'.
-		parts = []
-		part_boundary = '--' + self.boundary
-
-        # Add the form fields
-		parts.extend(
-			[ bytes(part_boundary, 'utf-8'),
-			bytes('Content-Disposition: form-data; name="%s"' % name, 'utf-8'),
-			b'',
-			bytes(value, 'utf-8'),
-			]
-			for name, value in self.form_fields
-			)
-
-        # Add the files to upload
-		parts.extend(
-			[ bytes(part_boundary, 'utf-8'),
-			bytes('Content-Disposition: file; name="%s"; filename="%s"' % (field_name, filename), 'utf-8'),
-			bytes('Content-Type: %s' % content_type, 'utf-8'),
-			b'',
-			body,
-			]
-			for field_name, filename, content_type, body in self.files
-			)
-
-        # Flatten the list and add closing boundary marker,
-        # then return CR+LF separated data
-		flattened = list(itertools.chain(*parts))
-		flattened.append(bytes('--' + self.boundary + '--', 'utf-8'))
-		flattened.append(b'')
-		self.form_data = b'\r\n'.join(flattened)
-
-	#def __str__(self):
-		#return str({'self.form_fields': self.form_fields, 'self.files': '{} pieces'.format(len(self.files)), 'self.boundary': self.boundary})
-
-    # def __repr__(self):
-    # return str({'self.form_fields': self.form_fields, 'self.files': '{} pieces'.format(len(self.files)), 'self.boundary': self.boundary})
-        
-
-
-
-
 
 
